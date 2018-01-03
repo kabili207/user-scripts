3e582258cff890ba66ec071ec20cae79e7f81dfa
Andrew Nagle <kabili@zyrenth.com>
2018-01-03 11:05:33 -0500

    Select white or black foreground depending on background

--- Roundcube.user.js
+++ Roundcube.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Roundcube Tweaks
 // @namespace    http://zyrenth.com/
-// @version      0.4
+// @version      0.5
 // @description  Tweaks to roundcube
 // @author       Andrew Nagle
 // @match        https://mail.zyrenth.com/*
@@ -48,30 +48,48 @@
         return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
     }
 
+    function getForegroundColor(bgColor, lightColor, darkColor) {
+        var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+        var r = parseInt(color.substring(0, 2), 16); // hexToR
+        var g = parseInt(color.substring(2, 4), 16); // hexToG
+        var b = parseInt(color.substring(4, 6), 16); // hexToB
+        var uicolors = [r / 255, g / 255, b / 255];
+        var c = uicolors.map((col) => {
+            if (col <= 0.03928) {
+                return col / 12.92;
+            }
+            return Math.pow((col + 0.055) / 1.055, 2.4);
+        });
+        var L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
+        return (L > 0.179) ? darkColor : lightColor;
+    }
+
     addGlobalStyle(".messagelist tr.unread span.tb_label_dots { font-weight: normal; }");
     addGlobalStyle(
         "#messagelist tr.message td.subject { position: relative; }" +
         "#messagelist tr.message td.subject span.tb_label_dots { top: 0.75em; } "
     );
-    
+
     var myFlags = GM_SuperValue.get("FlagConfig", [
         { flag:"STORY", color:"#33CCFF" },
         { flag:"HENTAI", color:"#FF6666" },
-        { flag:"GOO", color:"#CC33FF" }
+        { flag:"GOO", color:"#CC33FF" },
+        { flag:"LOYALTY", color:"#000000" }
     ]);
 
     for (var i = 0; i < myFlags.length; i++) {
         var flagName = myFlags[i].flag;
         var flagColor = myFlags[i].color;
         var tagName = "tb_label_" + flagName;
+        var foreColor = getForegroundColor(flagColor, '#FFFFFF', '#000000');
 
         addGlobalStyle(
             ".tb_label_dots span." + tagName + " { color: " + flagColor +"; } " +
             "#messagelist tr.selected." + tagName + " td, #messagelist tr.selected." + tagName + " td a { color: #FFFFFF; background-color: " + flagColor + "; } " +
             "#messagelist tr." + tagName + " td, #messagelist tr." + tagName + " td a, .toolbarmenu li." + tagName + ", .toolbarmenu li." + tagName + " a.active { color: " + flagColor + "; } " +
-            "div#labelbox span.box_" + tagName + " { background-color: " + flagColor + "; } "
+            "div#labelbox span.box_" + tagName + " { color: " + foreColor + "; background-color: " + flagColor + "; } "
         );
-        
+
         var labelbox = document.querySelector("div#labelbox span.box_" + tagName);
         if(labelbox) {
             labelbox.innerHTML = flagName.toLowerCase();
